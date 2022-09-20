5ab54cdbb575f3d79d9a803cfaddb4ff28795a73
gpradinett <gpradinett@gmail.com>
2022-09-19 22:07:29 -0500

    ✅ errors that returns a JSON-formatted 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Inicialization app.py"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Inicialization teardown_appcontext"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """ json 404 page """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
